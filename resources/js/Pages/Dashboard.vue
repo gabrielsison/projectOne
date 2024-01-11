e5da672de08479b35810baaad9c5ab1955a44390
Gab <gabsison@icloud.com>
2024-01-11 15:03:48 +0000

    view added

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,28 +1,34 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import { Head } from '@inertiajs/vue3'
-import Map from '@/Components/Map.vue'
-import Data from '@/Pages/Partials/Data.vue'
+import { Head, Link } from '@inertiajs/vue3'
+
+const lastUpdated = new Date().toLocaleString()
+console.log(first)
 </script>
 
 <template>
    <Head title="Dashboard" />
 
    <AuthenticatedLayout>
-      <div class="flex flex-col-reverse grow">
-         <section class="w-full">
-            <!--Breadcrumbs-->
-            <div className="text-sm breadcrumbs ml-4 mt-4 w-fit flex flex-col">
-               <ul>
-                  <li><a>Home</a></li>
-                  <li>QTR XY123Z</li>
-               </ul>
+      <div className="card w-80 m-4 bg-base-100 shadow-xl">
+         <figure>
+            <img
+               src="https://cdn.jetphotos.com/full/6/1064350_1700504515.jpg"
+               alt=""
+            />
+         </figure>
+         <div className="card-body">
+            <h2 className="card-title">QTR XY123Z</h2>
+            <p class="text-sm">Last Updated: {{ lastUpdated }}</p>
+            <div className="card-actions justify-end">
+               <Link
+                  :href="route('plane.view', 1)"
+                  className="btn btn-primary btn-wide mx-auto mt-4"
+               >
+                  Visit
+               </Link>
             </div>
-            <!--Data-->
-            <Data />
-         </section>
-         <!--Map-->
-         <Map />
+         </div>
       </div>
    </AuthenticatedLayout>
 </template>
---
+++ resources/js/Pages/Plane/View.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head, Link } from '@inertiajs/vue3'
+import Map from '@/Components/Map.vue'
+import Data from '@/Pages/Partials/Data.vue'
+</script>
+
+<template>
+   <Head title="Dashboard" />
+
+   <AuthenticatedLayout>
+      <div class="flex flex-col-reverse grow">
+         <section class="w-full">
+            <!--Breadcrumbs-->
+            <div className="text-sm breadcrumbs ml-4 mt-4 w-fit flex flex-col">
+               <ul>
+                  <li><Link :href="route('dashboard')">Home</Link></li>
+                  <li>QTR XY123Z</li>
+               </ul>
+            </div>
+            <!--Data-->
+            <Data />
+         </section>
+         <!--Map-->
+         <Map />
+      </div>
+   </AuthenticatedLayout>
+</template>
